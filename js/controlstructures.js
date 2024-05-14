1a600374effe6cf5c50fa224fe7561fec8d96c2b
HOSEWELL <hosewellkaranja@gmail.com>
2024-05-14 14:19:30 +0300

    new files

---
+++ js/controlstructures.js
@@ -0,0 +1,40 @@
+{
+    let x=30;
+   if(x<50) {
+    console.log("x is less 50");
+   }else
+   console.log("x  is not less than 50");
+}
+{
+   let number = 54; 
+   if(number % 2== 0){
+    console.log( number + " is even number");
+   }else{
+    console.log( number + " not even number");
+   }
+}
+
+
+{
+    let grade;
+    let mark= 30;
+
+    if(mark>=85){
+        grade= "A";
+    }else if( mark>=70){
+        grade = "B";
+    }else if(mark>=60){
+        grade = "C";
+    }else if(mark>=50){
+        grade = "D";
+    }else {
+        grade = "E";
+    }
+    console.log(grade);
+}
+{
+let number=2025;
+if(number%4==0){
+    console.log(number +" not reap year")
+}else console.log(number + " not reap year")
+}
---
+++ js/datatype.js
@@ -0,0 +1,38 @@
+{let names = "Daniel"+"Jones";
+console.log(names);
+}
+{
+    let b = 24+7+"egg"
+    console.log(b);
+    let c = "egg"+ 24 + 7;
+    console.log(c);
+    console.log("\n");
+}
+{
+
+}
+{
+    console.log(4>7);
+    console.log(50>4);
+    console.log(3<10);
+    console.log(3==10);
+    console.log(0.5==1/2);
+    console.log(3<=10);
+    console.log(10!=10);
+    console.log("jane">"dan");
+    console.log("a">"b");
+    console.log("abel">"ann");
+    console.log("name"<6);
+    console.log("5"==5);
+    console.log("5">3);
+
+}
+{
+  let x=45;
+  if(x>10){
+    console.log("x is grater than 10");
+  }else{
+    (x)
+  }
+  
+}
